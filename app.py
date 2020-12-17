ba1111e949f75a504c2df22e5b0e56658361d3a8
ElJefe111944 <jamesfarr1@sky.com>
2020-12-17 15:20:21 +0000

    Add Materilaize and Static files js and css

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def get_recipes():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=True) # change to False for submission
