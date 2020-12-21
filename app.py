51b65e00498c2066ce81b4c2bb3d158794d01415
ElJefe111944 <jamesfarr1@sky.com>
2020-12-21 19:09:23 +0000

    recipe.html created

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-# Home
+# Home Page
 @app.route("/")
 @app.route('/home_page')
 def home_page():
@@ -29,19 +29,36 @@ def home_page():
 
 
 
-# all recipes
+# All recipes
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
 
-
+# Search Bar 
 @app.route("/search", methods=["GET", "POST"])
 def search():
     search_recipe = request.form.get("search_recipe")
     recipes = mongo.db.recipes.find({"$text": {"$search": search_recipe}})
     return render_template("recipes.html", recipes=recipes)
+
+"""
+# Filter
+@app.route("/filter", methods=["GET", "POST"])
+def filter():
+    # find category names from mongodb
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    # Select by category name 
+    category_name_search = request.form.get("category_name_search")
+    recipes = mongo.db.recipes.find({
+        "$text": {"$search": '"'+category_name_search+'"'}}).sort("recipe_name", 1)
+    
+    # Search results found
+    return render_template("recipes.html", recipe_name=recipe_name,
+    category_name=category_name)
+"""
+
 
 
 if __name__ == "__main__":
