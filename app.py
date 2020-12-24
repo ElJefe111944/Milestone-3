b3645c817ba778f8158bccd569cff0d9e41dab85
ElJefe111944 <jamesfarr1@sky.com>
2020-12-24 12:02:53 +0000

    Registration page created ans styled

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -67,6 +68,10 @@ def filter():
     category_name=category_name)
 """
 
+# Registration 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
